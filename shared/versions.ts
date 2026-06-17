8063b0d8ad350631df5e65841a19d5c8823d1365
miyin <zouchaolive@gmail.com>
2026-06-17 22:43:38 +0800

    chore bump release versions

--- shared/versions.ts
+++ shared/versions.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.3.145";
+export const APP_VERSION = "2.3.146";
 export const ANDROID_APP_VERSION = "2.3.44";
-export const ANDROID_APK_RELEASE_VERSION = "2.3.145";
-export const AGENT_VERSION = "2.2.95";
+export const ANDROID_APK_RELEASE_VERSION = "2.3.146";
+export const AGENT_VERSION = "2.2.96";
